42c22e8d964e0670d413908e49fb1dbe6d80eb4a
Hazeem Hassan <hhazimhassan@gmail.com>
2020-06-06 12:51:47 +0500

    Navigation Darwer -profile -notification -location service and permission

---
+++ app/src/main/java/com/example/database_project_salesman/notification_Activity.java
@@ -0,0 +1,14 @@
+package com.example.database_project_salesman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class notification_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+}
