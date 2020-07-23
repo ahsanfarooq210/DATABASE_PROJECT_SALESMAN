f2efdae8c71fba0df3bd2139da45550c0a8acf27
Ahsan <ahsanfarooq210@gmail.com>
2020-07-23 13:53:28 +0500

    edit model class

--- app/src/main/java/com/example/database_project_salesman/Target/Enity/Target_SalesMen.java
+++ app/src/main/java/com/example/database_project_salesman/Target/Enity/Target_SalesMen.java
@@ -2,10 +2,11 @@ package com.example.database_project_salesman.Target.Enity;
 
 
 import com.example.database_project_salesman.Order.Entity.Orders;
-import  com.example.database_project_salesman.SKU.Sku;
+import com.example.database_project_salesman.SKU.Sku;
 
 
-public class Target_SalesMen {
+public class Target_SalesMen
+{
     String TARGET_ID;
     String SKU_ID;
     int achieved;
@@ -14,78 +15,107 @@ public class Target_SalesMen {
     Sku Sku;
     Orders orders;
     String status;
-    public Target_SalesMen() {
+    String status_skuId;
+
+    public Target_SalesMen()
+    {
     }
 
-    public Target_SalesMen(String TARGET_ID, String SKU_ID, int achieved,String saleMenEmail, Sku sku, Orders orders,String status) {
+    public Target_SalesMen(String TARGET_ID, String SKU_ID, int achieved, String saleMenEmail, Sku sku, Orders orders, String status)
+    {
         this.TARGET_ID = TARGET_ID;
         this.SKU_ID = SKU_ID;
 
         this.achieved = achieved;
 
         SaleMenEmail = saleMenEmail;
-this.status=status;
+        this.status = status;
         Sku = sku;
         this.orders = orders;
 
+        status_skuId=status+SKU_ID;
+
     }
 
-    public String getStatus() {
+    public String getStatus_skuId()
+    {
+        return status_skuId;
+    }
+
+    public void setStatus_skuId(String status_skuId)
+    {
+        this.status_skuId = status_skuId;
+    }
+
+    public String getStatus()
+    {
         return status;
     }
 
-    public void setStatus(String status) {
+    public void setStatus(String status)
+    {
         this.status = status;
     }
 
-    public String getTARGET_ID() {
+    public String getTARGET_ID()
+    {
         return TARGET_ID;
     }
 
-    public void setTARGET_ID(String TARGET_ID) {
+    public void setTARGET_ID(String TARGET_ID)
+    {
         this.TARGET_ID = TARGET_ID;
     }
 
-    public String getSKU_ID() {
+    public String getSKU_ID()
+    {
         return SKU_ID;
     }
 
-    public void setSKU_ID(String SKU_ID) {
+    public void setSKU_ID(String SKU_ID)
+    {
         this.SKU_ID = SKU_ID;
     }
 
 
-
-    public int getAchieved() {
+    public int getAchieved()
+    {
         return achieved;
     }
 
-    public void setAchieved(int achieved) {
+    public void setAchieved(int achieved)
+    {
         this.achieved = achieved;
     }
 
-    public String getSaleMenEmail() {
+    public String getSaleMenEmail()
+    {
         return SaleMenEmail;
     }
 
-    public void setSaleMenEmail(String saleMenEmail) {
+    public void setSaleMenEmail(String saleMenEmail)
+    {
         SaleMenEmail = saleMenEmail;
     }
 
 
-    public com.example.database_project_salesman.SKU.Sku getSku() {
+    public com.example.database_project_salesman.SKU.Sku getSku()
+    {
         return Sku;
     }
 
-    public void setSku(com.example.database_project_salesman.SKU.Sku sku) {
+    public void setSku(com.example.database_project_salesman.SKU.Sku sku)
+    {
         Sku = sku;
     }
 
-    public Orders getOrders() {
+    public Orders getOrders()
+    {
         return orders;
     }
 
-    public void setOrders(Orders orders) {
+    public void setOrders(Orders orders)
+    {
         this.orders = orders;
     }
 
